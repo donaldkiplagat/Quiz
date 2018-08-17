200693f4a44ea97c6fbd7f354e10e2f630bcdbd3
Donald Kiplagat <donald.k.kiplagat@gmail.com>
2018-08-17 11:46:56 +0300

    Basic CSS and jQuery for hiding elements added

--- js/script.js
+++ js/script.js
@@ -1,21 +1,20 @@
 $(document).ready(function(){
-  $("form#options").submit(fucntion(event)
+  $("form#options").submit(function(event){
 
   var question1= $("input:radio[name=question1]:checked").val();
   var question2= $("input:radio[name=question2]:checked").val();
   var question3= $("input:radio[name=question3]:checked").val();
   var question4= $("input:radio[name=question4]:checked").val();
   var question5= $("input:radio[name=question5]:checked").val();
+  var result= parseInt(question1)+parseInt(question2)+parseInt(question3)+parseInt(question4)+parseInt(question5);//question1.concat("\n").concat(question2);
+  $("#result").text("Your answers are "+result);
 
-  var result= parseInt(question1)+parseInt(question2)+parseInt(question3)+parseInt(question4)+parseInt(question5);
-  $("#result").text("Your total marks are: "+result);
 
   $(".quiz").hide();
   $(".answer").show();
 
   event.preventDefault();
 
-
-
 });
+
 });
