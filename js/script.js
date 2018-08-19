2a5d4e7572994adac5be9814baa55c91b9cac96b
Donald Kiplagat <donald.k.kiplagat@gmail.com>
2018-08-19 16:02:31 +0300

    Entry validation added

--- js/script.js
+++ js/script.js
@@ -7,12 +7,17 @@ $(document).ready(function(){
   var question4= $("input:radio[name=question4]:checked").val();
   var question5= $("input:radio[name=question5]:checked").val();
   var result= parseInt(question1)+parseInt(question2)+parseInt(question3)+parseInt(question4)+parseInt(question5);//question1.concat("\n").concat(question2);
-  $("#result").text("Your total score is "+result+"/100");
 
+  if(isNaN(result)=== true){
+    $("#result").text("Please ensure all questions are answered");
+    $(".quiz").hide();
+    $(".answer").show();
+  }else{
 
-  $(".quiz").hide();
-  $(".answer").show();
-
+    $("#result").text("Your total score is "+result+"/100");
+    $(".quiz").hide();
+    $(".answer").show();
+  }
   event.preventDefault();
 
 });
