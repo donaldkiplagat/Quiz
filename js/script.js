3f7dedb00d65520dc4fef6dc7789ba55e2e6d875
Donald Kiplagat <donald.k.kiplagat@gmail.com>
2018-08-19 15:13:55 +0300

    Favicon added

--- js/script.js
+++ js/script.js
@@ -6,8 +6,10 @@ $(document).ready(function(){
   var question3= $("input:radio[name=question3]:checked").val();
   var question4= $("input:radio[name=question4]:checked").val();
   var question5= $("input:radio[name=question5]:checked").val();
+
+
   var result= parseInt(question1)+parseInt(question2)+parseInt(question3)+parseInt(question4)+parseInt(question5);//question1.concat("\n").concat(question2);
-  $("#result").text("Your answers are "+result);
+  $("#result").text("Your total score is "+result+"/100");
 
 
   $(".quiz").hide();
