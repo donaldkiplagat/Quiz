14b34666478fbafb965f6afec15f530c8c91a0d7
Donald Kiplagat <donald.k.kiplagat@gmail.com>
2018-08-19 16:12:39 +0300

    Indentation added to script.js file,Deletion of trial code,Addition of comments

--- js/script.js
+++ js/script.js
@@ -1,25 +1,24 @@
 $(document).ready(function(){
   $("form#options").submit(function(event){
 
-  var question1= $("input:radio[name=question1]:checked").val();
-  var question2= $("input:radio[name=question2]:checked").val();
-  var question3= $("input:radio[name=question3]:checked").val();
-  var question4= $("input:radio[name=question4]:checked").val();
-  var question5= $("input:radio[name=question5]:checked").val();
-  var result= parseInt(question1)+parseInt(question2)+parseInt(question3)+parseInt(question4)+parseInt(question5);//question1.concat("\n").concat(question2);
+    var question1= $("input:radio[name=question1]:checked").val();
+    var question2= $("input:radio[name=question2]:checked").val();
+    var question3= $("input:radio[name=question3]:checked").val();
+    var question4= $("input:radio[name=question4]:checked").val();
+    var question5= $("input:radio[name=question5]:checked").val();
+    var result= parseInt(question1)+parseInt(question2)+parseInt(question3)+parseInt(question4)+parseInt(question5);
 
-  if(isNaN(result)=== true){
-    $("#result").text("Please ensure all questions are answered");
-    $(".quiz").hide();
-    $(".answer").show();
-  }else{
+      //Checks if one of the answers is left blank
+      if(isNaN(result)=== true){
+        $("#result").text("Please ensure all questions are answered");
+        $(".quiz").hide();
+        $(".answer").show();
+      }else{
 
-    $("#result").text("Your total score is "+result+"/100");
-    $(".quiz").hide();
-    $(".answer").show();
-  }
+        $("#result").text("Your total score is "+result+"/100");
+        $(".quiz").hide();
+        $(".answer").show();
+      }
   event.preventDefault();
-
-});
-
+  });
 });
