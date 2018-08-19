4d7eaa927bf384394f44622c8d7c881eac8d9996
Donald Kiplagat <donald.k.kiplagat@gmail.com>
2018-08-19 15:45:02 +0300

    Background image added into container

--- js/script.js
+++ js/script.js
@@ -6,8 +6,6 @@ $(document).ready(function(){
   var question3= $("input:radio[name=question3]:checked").val();
   var question4= $("input:radio[name=question4]:checked").val();
   var question5= $("input:radio[name=question5]:checked").val();
-
-
   var result= parseInt(question1)+parseInt(question2)+parseInt(question3)+parseInt(question4)+parseInt(question5);//question1.concat("\n").concat(question2);
   $("#result").text("Your total score is "+result+"/100");
 
